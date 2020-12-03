00811920cf1b6b54cf1bde4b3bbf99a5de53a01a
Viacheslav Poturaev <vearutop@gmail.com>
2020-12-03 22:56:22 +0100

    Test report

--- noop.go
+++ noop.go
@@ -9,12 +9,12 @@ type NoOp struct{}
 
 var _ ReadWriter = NoOp{}
 
-// Read does not find anything.
-func (NoOp) Read(ctx context.Context, key string) (interface{}, error) {
-	return nil, ErrCacheItemNotFound
-}
-
 // Write discards value.
 func (NoOp) Write(ctx context.Context, key string, v interface{}) error {
 	return nil
+}
+
+// Read does not find anything.
+func (NoOp) Read(ctx context.Context, key string) (interface{}, error) {
+	return nil, ErrCacheItemNotFound
 }
