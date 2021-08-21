c0d05df570773a1d7f9924e293ad570c9fb8d829
Viacheslav Poturaev <vearutop@gmail.com>
2021-08-21 15:10:17 +0200

    Try xsync in benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,344 +1,364 @@
-package cache_test
+package cachex_test
 
 import (
 	"context"
+	"fmt"
+	"github.com/puzpuzpuz/xsync"
+	"math"
+	"runtime"
+	"runtime/debug"
 	"strconv"
 	"sync"
 	"testing"
 	"time"
 
 	"github.com/bool64/cache"
-	pca "github.com/patrickmn/go-cache"
 )
 
-func Benchmark_Memory(b *testing.B) {
-	c := cache.NewMemory()
-	ctx := context.Background()
+func Benchmark_concurrent(b *testing.B) {
+	for _, cardinality := range []int{1e6} {
+		cardinality := cardinality
 
-	b.ReportAllocs()
-	b.ResetTimer()
+		for _, numRoutines := range []int{ /*1, */ runtime.GOMAXPROCS(0)} {
+			numRoutines := numRoutines
+			for _, writePercent := range []float64{10} {
+				writeEvery := int(100.0 / writePercent)
 
-	for i := 0; i < b.N; i++ {
-		k := "oneone" + strconv.Itoa(i%10000)
-		// nolint
-		if i < 10000 {
-			_ = c.Write(ctx, k, 123)
-		}
-		// nolint
-		_, _ = c.Read(ctx, k)
-	}
-}
+				for _, loader := range []cacheLoader{
+					//failoverShardedMap{writeEvery: writeEvery},
+					//shardedMap{writeEvery: writeEvery},
+					syncMap{writeEvery: writeEvery},
+					//xSyncMap{writeEvery: writeEvery},
+				} {
+					loader := loader
 
-func Benchmark_SyncMap_concurrent(b *testing.B) {
-	c := cache.NewSyncMap()
-	ctx := context.Background()
+					b.Run(fmt.Sprintf("c%d:g%d:w%.2f%%:%T", cardinality, numRoutines, writePercent, loader), func(b *testing.B) {
+						before := heapInUse()
 
-	cardinality := 10000
-	for i := 0; i < cardinality; i++ {
-		k := "oneone" + strconv.Itoa(i)
-		_ = c.Write(ctx, k, 123)
-	}
+						c := loader.make(b, cardinality)
 
-	b.ReportAllocs()
-	b.ResetTimer()
+						b.ReportAllocs()
+						b.ResetTimer()
 
-	numRoutines := 50
-	wg := sync.WaitGroup{}
-	wg.Add(numRoutines)
+						wg := sync.WaitGroup{}
+						wg.Add(numRoutines)
 
-	for r := 0; r < numRoutines; r++ {
-		cnt := b.N / numRoutines
-		if r == 0 {
-			cnt = b.N - cnt*(numRoutines-1)
-		}
-		go func() {
-			for i := 0; i < cnt; i++ {
-				k := "oneone" + strconv.Itoa((i^12345)%cardinality)
-				v, _ := c.Read(ctx, k)
-				if v.(int) != 123 {
-					b.Fail()
+						for r := 0; r < numRoutines; r++ {
+							cnt := b.N / numRoutines
+							if r == 0 {
+								cnt = b.N - cnt*(numRoutines-1)
+							}
+
+							go func() {
+								c.run(b, cnt)
+								wg.Done()
+							}()
+						}
+
+						wg.Wait()
+						b.StopTimer()
+						b.ReportMetric(float64(heapInUse()-before)/(1024*1024), "MB/inuse")
+						fmt.Sprintln(c)
+					})
 				}
 			}
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-}
-
-func Benchmark_Memory_concurrent(b *testing.B) {
-	c := cache.NewMemory()
-	ctx := context.Background()
-
-	cardinality := 10000
-	for i := 0; i < cardinality; i++ {
-		k := "oneone" + strconv.Itoa(i)
-		_ = c.Write(ctx, k, 123)
-	}
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	numRoutines := 50
-	wg := sync.WaitGroup{}
-	wg.Add(numRoutines)
-
-	for r := 0; r < numRoutines; r++ {
-		cnt := b.N / numRoutines
-		if r == 0 {
-			cnt = b.N - cnt*(numRoutines-1)
 		}
-		go func() {
-			for i := 0; i < cnt; i++ {
-				k := "oneone" + strconv.Itoa((i^12345)%cardinality)
-				v, _ := c.Read(ctx, k)
-				if v.(int) != 123 {
-					b.Fail()
-				}
-			}
-			wg.Done()
-		}()
 	}
-
-	wg.Wait()
 }
 
-func Benchmark_MutexMap_concurrent(b *testing.B) {
-	c := cache.NewMutexMap()
-	ctx := context.Background()
+// cachedValue represents a small value for a cached item.
+type smallCachedValue struct {
+	b bool
+	s string
+	i int
+}
 
-	cardinality := 10000
-	for i := 0; i < cardinality; i++ {
-		k := "oneone" + strconv.Itoa(i)
-		_ = c.Write(ctx, k, 123)
+func makeCachedValue(i int) smallCachedValue {
+	return smallCachedValue{
+		i: i,
+		s: longString + strconv.Itoa(i),
+		b: true,
 	}
+}
 
-	b.ReportAllocs()
-	b.ResetTimer()
+func init() {
+	cache.GobRegister(smallCachedValue{})
+}
 
-	numRoutines := 50
-	wg := sync.WaitGroup{}
-	wg.Add(numRoutines)
+const (
+	longString = "looooooooooooooooooooooooooongstring"
+	keyPrefix  = "thekey"
+)
 
-	for r := 0; r < numRoutines; r++ {
-		cnt := b.N / numRoutines
-		if r == 0 {
-			cnt = b.N - cnt*(numRoutines-1)
+type cacheLoader interface {
+	make(b *testing.B, cardinality int) cacheLoader
+	run(b *testing.B, cnt int)
+}
+
+func heapInUse() uint64 {
+	var (
+		m         = runtime.MemStats{}
+		prevInUse uint64
+	)
+
+	for {
+		runtime.ReadMemStats(&m)
+
+		if math.Abs(float64(m.HeapInuse-prevInUse)) < 1*1024 {
+			break
 		}
-		go func() {
-			for i := 0; i < cnt; i++ {
-				k := "oneone" + strconv.Itoa((i^12345)%cardinality)
-				v, _ := c.Read(ctx, k)
-				if v.(int) != 123 {
-					b.Fail()
-				}
-			}
-			wg.Done()
-		}()
+
+		prevInUse = m.HeapInuse
+
+		time.Sleep(50 * time.Millisecond)
+		runtime.GC()
+		debug.FreeOSMemory()
 	}
 
-	wg.Wait()
+	return m.HeapInuse
 }
 
-func Benchmark_ShardedMap_concurrent(b *testing.B) {
-	c := cache.NewShardedMap()
+type failoverShardedMap struct {
+	c           *cache.Failover
+	cardinality int
+	writeEvery  int
+}
+
+func (sbm failoverShardedMap) make(b *testing.B, cardinality int) cacheLoader {
+	b.Helper()
+
+	u := cache.NewShardedMap()
 	ctx := context.Background()
-
-	cardinality := 10000
-	for i := 0; i < cardinality; i++ {
-		k := "oneone" + strconv.Itoa(i)
-		_ = c.Write(ctx, k, 123)
-	}
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	numRoutines := 50
-	wg := sync.WaitGroup{}
-	wg.Add(numRoutines)
-
-	for r := 0; r < numRoutines; r++ {
-		cnt := b.N / numRoutines
-		if r == 0 {
-			cnt = b.N - cnt*(numRoutines-1)
-		}
-		go func() {
-			for i := 0; i < cnt; i++ {
-				k := "oneone" + strconv.Itoa((i^12345)%cardinality)
-				v, _ := c.Read(ctx, k)
-				if v.(int) != 123 {
-					b.Fail()
-				}
-			}
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-}
-
-func Benchmark_ShardedByteMap_concurrent(b *testing.B) {
-	c := cache.NewShardedByteMap()
-	ctx := context.Background()
-
-	cardinality := 10000
-	for i := 0; i < cardinality; i++ {
-		k := "oneone" + strconv.Itoa(i)
-		_ = c.Write(ctx, []byte(k), 123)
-	}
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	numRoutines := 50
-	wg := sync.WaitGroup{}
-	wg.Add(numRoutines)
-
-	for r := 0; r < numRoutines; r++ {
-		cnt := b.N / numRoutines
-		if r == 0 {
-			cnt = b.N - cnt*(numRoutines-1)
-		}
-		go func() {
-			for i := 0; i < cnt; i++ {
-				k := "oneone" + strconv.Itoa((i^12345)%cardinality)
-				v, _ := c.Read(ctx, []byte(k))
-				if v.(int) != 123 {
-					b.Fail()
-				}
-			}
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-}
-
-func Benchmark_Patrickmn_concurrent(b *testing.B) {
-	c := pca.New(5*time.Minute, 10*time.Minute)
-
-	cardinality := 10000
-	for i := 0; i < cardinality; i++ {
-		k := "oneone" + strconv.Itoa(i)
-		c.Set(k, 123, time.Minute)
-	}
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	numRoutines := 50
-	wg := sync.WaitGroup{}
-	wg.Add(numRoutines)
-
-	for r := 0; r < numRoutines; r++ {
-		cnt := b.N / numRoutines
-		if r == 0 {
-			cnt = b.N - cnt*(numRoutines-1)
-		}
-		go func() {
-			for i := 0; i < cnt; i++ {
-				k := "oneone" + strconv.Itoa((i^12345)%cardinality)
-				v, _ := c.Get(k)
-				if v.(int) != 123 {
-					b.Fail()
-				}
-			}
-			wg.Done()
-		}()
-	}
-
-	wg.Wait()
-}
-
-func Benchmark_SyncMap(b *testing.B) {
-	c := cache.NewSyncMap()
-	ctx := context.Background()
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		k := "oneone" + strconv.Itoa(i%10000)
-		// nolint
-		if i < 10000 {
-			_ = c.Write(ctx, k, 123)
-		}
-		// nolint
-		_, _ = c.Read(ctx, k)
-	}
-}
-
-func Benchmark_Failover(b *testing.B) {
-	c := cache.NewFailover(cache.FailoverConfig{})
-	ctx := context.Background()
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		k := "oneone" + strconv.Itoa(i%10000)
-		// nolint
-		_, _ = c.Get(ctx, k, func(ctx context.Context) (interface{}, error) {
-			return 123, nil
-		})
-	}
-}
-
-func Benchmark_FailoverSyncRead(b *testing.B) {
-	c := cache.NewFailover(cache.FailoverConfig{
-		SyncRead: true,
+	c := cache.NewFailover(func(cfg *cache.FailoverConfig) {
+		cfg.Backend = u
 	})
-	ctx := context.Background()
+	buf := make([]byte, 0)
 
-	b.ReportAllocs()
-	b.ResetTimer()
+	for i := 0; i < cardinality; i++ {
+		i := i
 
-	for i := 0; i < b.N; i++ {
-		k := "oneone" + strconv.Itoa(i%10000)
-		// nolint
-		_, _ = c.Get(ctx, k, func(ctx context.Context) (interface{}, error) {
-			return 123, nil
+		buf = append(buf[:0], []byte(keyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		_, err := c.Get(ctx, buf, func(ctx context.Context) (interface{}, error) {
+			return makeCachedValue(i), nil
 		})
-	}
-}
-
-// Benchmark_StringKeyPatrickmn is archived.
-// Add import `pca "github.com/patrickmn/go-cache"` to enable it.
-// Sample result:
-// Benchmark_Memory-16                 	 6299344	       180 ns/op	      16 B/op	       1 allocs/op
-// Benchmark_Failover-16               	 5991889	       215 ns/op	      16 B/op	       1 allocs/op
-// Benchmark_FailoverSyncRead-16       	 3199423	       355 ns/op	     113 B/op	       3 allocs/op
-// Benchmark_FailoverAlwaysBuild-16    	 1000000	      1134 ns/op	     523 B/op	       7 allocs/op
-// Benchmark_Patrickmn-4          	     5000000	       258 ns/op	      16 B/op	       1 allocs/op
-//*
-func Benchmark_Patrickmn(b *testing.B) {
-	c := pca.New(5*time.Minute, 10*time.Minute)
-
-	b.ReportAllocs()
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		k := "oneone" + strconv.Itoa(i%10000)
-		if i < 10000 {
-			c.Set(k, 123, time.Minute)
+		if err != nil {
+			b.Fail()
 		}
-		_, _ = c.Get(k)
+	}
+
+	return failoverShardedMap{
+		c:           c,
+		cardinality: cardinality,
 	}
 }
 
-//*/
+func (sbm failoverShardedMap) run(b *testing.B, cnt int) {
+	b.Helper()
 
-func Benchmark_FailoverAlwaysBuild(b *testing.B) {
-	c := cache.NewFailover(cache.FailoverConfig{})
 	ctx := context.Background()
+	buf := make([]byte, 0, 10)
 
-	b.ReportAllocs()
-	b.ResetTimer()
+	w := 0
+	for i := 0; i < cnt; i++ {
+		i := (i ^ 12345) % sbm.cardinality
 
-	for i := 0; i < b.N; i++ {
-		k := "oneone" + strconv.Itoa(i)
-		// nolint
-		_, _ = c.Get(ctx, k, func(ctx context.Context) (interface{}, error) {
-			return 123, nil
+		buf = append(buf[:0], []byte(keyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		w++
+		if w == sbm.writeEvery {
+			w = 0
+			v, err := sbm.c.Get(cache.WithSkipRead(ctx), buf, func(ctx context.Context) (interface{}, error) {
+				return smallCachedValue{}, nil
+			})
+
+			if v.(smallCachedValue).i != i || err != nil {
+				b.Fail()
+			}
+
+			continue
+		}
+
+		v, err := sbm.c.Get(ctx, buf, func(ctx context.Context) (interface{}, error) {
+			return smallCachedValue{}, nil
 		})
+
+		if v.(smallCachedValue).i != i || err != nil {
+			b.Fail()
+		}
+	}
+}
+
+type shardedMap struct {
+	c           *cache.ShardedMap
+	cardinality int
+	writeEvery  int
+}
+
+func (sbm shardedMap) make(b *testing.B, cardinality int) cacheLoader {
+	b.Helper()
+
+	ctx := context.Background()
+	c := cache.NewShardedMap()
+	buf := make([]byte, 0)
+
+	for i := 0; i < cardinality; i++ {
+		i := i
+
+		buf = append(buf[:0], []byte(keyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		err := c.Write(ctx, buf, makeCachedValue(i))
+		if err != nil {
+			b.Fail()
+		}
+	}
+
+	return shardedMap{
+		c:           c,
+		cardinality: cardinality,
+	}
+}
+
+func (sbm shardedMap) run(b *testing.B, cnt int) {
+	b.Helper()
+
+	ctx := context.Background()
+	buf := make([]byte, 0, 10)
+
+	w := 0
+	for i := 0; i < cnt; i++ {
+		i := (i ^ 12345) % sbm.cardinality
+
+		buf = append(buf[:0], []byte(keyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		w++
+		if w == sbm.writeEvery {
+			w = 0
+			err := sbm.c.Write(ctx, buf, makeCachedValue(i))
+
+			if err != nil {
+				b.Fail()
+			}
+
+			continue
+		}
+
+		v, err := sbm.c.Read(ctx, buf)
+
+		if v.(smallCachedValue).i != i || err != nil {
+			b.Fail()
+		}
+	}
+}
+
+type syncMap struct {
+	c           *sync.Map
+	cardinality int
+	writeEvery  int
+}
+
+func (sbm syncMap) make(b *testing.B, cardinality int) cacheLoader {
+	b.Helper()
+
+	c := sync.Map{}
+	buf := make([]byte, 0)
+
+	for i := 0; i < cardinality; i++ {
+		i := i
+
+		buf = append(buf[:0], []byte(keyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		c.Store(string(buf), makeCachedValue(i))
+	}
+
+	return syncMap{
+		c:           &c,
+		cardinality: cardinality,
+	}
+}
+
+func (sbm syncMap) run(b *testing.B, cnt int) {
+	b.Helper()
+
+	buf := make([]byte, 0, 10)
+
+	w := 0
+	for i := 0; i < cnt; i++ {
+		i := (i ^ 12345) % sbm.cardinality
+
+		buf = append(buf[:0], []byte(keyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		w++
+		if w == sbm.writeEvery {
+			w = 0
+			sbm.c.Store(string(buf), makeCachedValue(i))
+			continue
+		}
+
+		v, ok := sbm.c.Load(string(buf))
+
+		if !ok || v.(smallCachedValue).i != i {
+			b.Fail()
+		}
+	}
+}
+
+type xSyncMap struct {
+	c           *xsync.Map
+	cardinality int
+	writeEvery  int
+}
+
+func (sbm xSyncMap) make(b *testing.B, cardinality int) cacheLoader {
+	b.Helper()
+
+	c := xsync.Map{}
+	buf := make([]byte, 0)
+
+	for i := 0; i < cardinality; i++ {
+		i := i
+
+		buf = append(buf[:0], []byte(keyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		c.Store(string(buf), makeCachedValue(i))
+	}
+
+	return xSyncMap{
+		c:           &c,
+		cardinality: cardinality,
+	}
+}
+
+func (sbm xSyncMap) run(b *testing.B, cnt int) {
+	b.Helper()
+
+	buf := make([]byte, 0, 10)
+
+	w := 0
+	for i := 0; i < cnt; i++ {
+		i := (i ^ 12345) % sbm.cardinality
+
+		buf = append(buf[:0], []byte(keyPrefix)...)
+		buf = append(buf, []byte(strconv.Itoa(i))...)
+
+		w++
+		if w == sbm.writeEvery {
+			w = 0
+			sbm.c.Store(string(buf), makeCachedValue(i))
+			continue
+		}
+
+		v, ok := sbm.c.Load(string(buf))
+
+		if !ok || v.(smallCachedValue).i != i {
+			b.Fail()
+		}
 	}
 }
---
+++ xsync_map.go
@@ -0,0 +1,301 @@
+package cachex
+
+import (
+	"context"
+	"encoding/gob"
+	"errors"
+	"github.com/bool64/cache"
+	"github.com/puzpuzpuz/xsync"
+	"io"
+	"math/rand"
+	"runtime"
+	"sort"
+	"time"
+)
+
+var (
+	_ cache.ReadWriter = &xSyncMap{}
+	_ cache.Deleter    = &xSyncMap{}
+)
+
+// XSyncMap is an in-memory cache backend. Please use NewXSyncMap to create it.
+type XSyncMap struct {
+	*xSyncMap
+}
+
+type xSyncMap struct {
+	data xsync.Map
+
+	*trait
+}
+
+// NewXSyncMap creates an instance of in-memory cache with optional configuration.
+func NewXSyncMap(options ...func(cfg *Config)) *XSyncMap {
+	c := &xSyncMap{}
+	C := &XSyncMap{
+		xSyncMap: c,
+	}
+
+	cfg := Config{}
+	for _, option := range options {
+		option(&cfg)
+	}
+
+	c.trait = newTrait(c, cfg)
+
+	runtime.SetFinalizer(C, func(m *XSyncMap) {
+		close(m.closed)
+	})
+
+	return C
+}
+
+// Read gets value.
+func (c *xSyncMap) Read(ctx context.Context, key []byte) (interface{}, error) {
+	if cache.SkipRead(ctx) {
+		return nil, cache.ErrNotFound
+	}
+
+	cacheEntry, found := c.data.Load(string(key))
+
+	return c.prepareRead(ctx, cacheEntry, found)
+}
+
+// Write sets value.
+func (c *xSyncMap) Write(ctx context.Context, k []byte, v interface{}) error {
+	// ttl := c.config.TimeToLive
+	ttl := cache.TTL(ctx)
+	if ttl == cache.DefaultTTL {
+		ttl = c.config.TimeToLive
+	}
+
+	if c.config.ExpirationJitter > 0 {
+		ttl += time.Duration(float64(ttl) * c.config.ExpirationJitter * (rand.Float64() - 0.5)) // nolint:gosec
+	}
+
+	// Copy key to allow mutations of original argument.
+	key := make([]byte, len(k))
+	copy(key, k)
+
+	c.data.Store(string(k), &entry{V: v, K: key, E: time.Now().Add(ttl)})
+
+	if c.log != nil {
+		c.log.Debug(ctx, "wrote to cache",
+			"name", c.config.Name,
+			"key", string(key),
+			"value", v,
+			"ttl", ttl,
+		)
+	}
+
+	if c.stat != nil {
+		c.stat.Add(ctx, cache.MetricWrite, 1, "name", c.config.Name)
+	}
+
+	return nil
+}
+
+func (c *xSyncMap) Delete(ctx context.Context, key []byte) error {
+	_, found := c.data.LoadAndDelete(string(key))
+	if !found {
+		return cache.ErrNotFound
+	}
+
+	if c.log != nil {
+		c.log.Debug(ctx, "deleted cache entry",
+			"name", c.config.Name,
+			"key", string(key),
+		)
+	}
+
+	return nil
+}
+
+// ExpireAll marks all entries as expired, they can still serve stale cache.
+func (c *xSyncMap) ExpireAll(ctx context.Context) {
+	now := time.Now()
+	cnt := 0
+
+	c.data.Range(func(key string, value interface{}) bool {
+		cacheEntry := value.(*entry)
+		cacheEntry.E = now
+		cnt++
+
+		return true
+	})
+
+	if c.log != nil {
+		c.log.Important(ctx, "expired all entries in cache",
+			"name", c.config.Name,
+			"elapsed", time.Since(now).String(),
+			"count", cnt,
+		)
+	}
+}
+
+// DeleteAll erases all entries.
+func (c *xSyncMap) DeleteAll(ctx context.Context) {
+	now := time.Now()
+	cnt := 0
+
+	c.data.Range(func(key string, _ interface{}) bool {
+		c.data.Delete(key)
+		cnt++
+
+		return true
+	})
+
+	if c.log != nil {
+		c.log.Important(ctx, "deleted all entries in cache",
+			"name", c.config.Name,
+			"elapsed", time.Since(now).String(),
+			"count", cnt,
+		)
+	}
+}
+
+func (c *xSyncMap) deleteExpiredBefore(expirationBoundary time.Time) {
+	c.data.Range(func(key string, value interface{}) bool {
+		cacheEntry := value.(*entry)
+		if cacheEntry.E.Before(expirationBoundary) {
+			c.data.Delete(key)
+		}
+
+		return true
+	})
+
+	if c.heapInUseOverflow() || c.countOverflow() {
+		c.evictOldest()
+	}
+}
+
+// Len returns number of elements in cache.
+func (c *xSyncMap) Len() int {
+	cnt := 0
+
+	c.data.Range(func(key string, value interface{}) bool {
+		cnt++
+
+		return true
+	})
+
+	return cnt
+}
+
+// Walk walks cached entries.
+func (c *xSyncMap) Walk(walkFn func(e cache.Entry) error) (int, error) {
+	n := 0
+	var lastErr error
+
+	c.data.Range(func(key string, value interface{}) bool {
+		err := walkFn(value.(*entry))
+		if err != nil {
+			lastErr = err
+			return false
+		}
+
+		n++
+
+		return true
+	})
+
+	return n, lastErr
+}
+
+// Dump saves cached entries and returns a number of processed entries.
+//
+// Dump uses encoding/gob to serialize cache entries, therefore it is necessary to
+// register cached types in advance with cache.GobRegister.
+func (c *XSyncMap) Dump(w io.Writer) (int, error) {
+	encoder := gob.NewEncoder(w)
+
+	return c.Walk(func(e cache.Entry) error {
+		return encoder.Encode(e)
+	})
+}
+
+// Restore loads cached entries and returns number of processed entries.
+//
+// Restore uses encoding/gob to unserialize cache entries, therefore it is necessary to
+// register cached types in advance with cache.GobRegister.
+func (c *XSyncMap) Restore(r io.Reader) (int, error) {
+	var (
+		decoder = gob.NewDecoder(r)
+		e       entry
+		n       = 0
+	)
+
+	for {
+		err := decoder.Decode(&e)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return n, err
+		}
+
+		c.data.Store(string(e.K), &e)
+
+		n++
+	}
+
+	return n, nil
+}
+
+func (c *xSyncMap) evictOldest() {
+	evictFraction := c.config.EvictFraction
+	if evictFraction == 0 {
+		evictFraction = 0.1
+	}
+
+	type en struct {
+		key      string
+		expireAt time.Time
+	}
+
+	keysCnt := c.Len()
+	entries := make([]en, 0, keysCnt)
+
+	// Collect all keys and expirations.
+	c.data.Range(func(key string, value interface{}) bool {
+		i := value.(*entry)
+		entries = append(entries, en{expireAt: i.E, key: string(i.K)})
+
+		return true
+	})
+
+	// Sort entries to put most expired in head.
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].expireAt.Before(entries[j].expireAt)
+	})
+
+	evictItems := int(float64(len(entries)) * evictFraction)
+
+	if c.stat != nil {
+		c.stat.Add(context.Background(), cache.MetricEvict, float64(evictItems), "name", c.config.Name)
+	}
+
+	for i := 0; i < evictItems; i++ {
+		c.data.Delete(entries[i].key)
+	}
+}
+
+func (c *xSyncMap) heapInUseOverflow() bool {
+	if c.config.HeapInUseSoftLimit == 0 {
+		return false
+	}
+
+	m := runtime.MemStats{}
+	runtime.ReadMemStats(&m)
+
+	return m.HeapInuse >= c.config.HeapInUseSoftLimit
+}
+
+func (c *xSyncMap) countOverflow() bool {
+	if c.config.CountSoftLimit == 0 {
+		return false
+	}
+
+	return c.Len() >= int(c.config.CountSoftLimit)
+}
